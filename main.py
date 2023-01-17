7ec5028078e841508ba9478dfd1def057d7321aa
Dylan <Dmactavishf@gmail.com>
2023-01-17 14:27:21 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+
+@app.route("/about_cosmyc_arcade", methods=["POST", "GET"])
+def about_cosmyc_arcade():
+    return render_template("aboutcosmycarcade.html")
+
+
+@app.route("/CosMycArcade")
+def play_cosmycarcade():
+    return render_template("CosMyc.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
